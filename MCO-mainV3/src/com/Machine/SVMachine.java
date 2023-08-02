3ed88870d699eb658753b59bf66797b6414b9a10
LazyXTwo <137690003+LazyXTwo@users.noreply.github.com>
2023-08-02 16:09:55 +0800

    Update SVMachine.java

--- MCO-mainV3/src/com/Machine/SVMachine.java
+++ MCO-mainV3/src/com/Machine/SVMachine.java
@@ -23,6 +23,33 @@ public class SVMachine extends Machine {
         return true;
     }
 
+    public void initStartingInventoryS() {
+        for (int i = 0; i < nonSellableItemsList.size(); i++) {
+            transactionSummary.get(i).setName(nonSellableItemsList.get(i).getName());
+            transactionSummary.get(i).setPrice(nonSellableItemsList.get(i).getPrice());
+            transactionSummary.get(i).setQuantity(nonSellableItemsList.get(i).getQuantity());
+        }
+    }
+    public String getItemNameS(int nIndex) {
+        return nonSellableItemsList.get(nIndex).getName();
+    }
+
+    public double getItemPrice(int nIndex) {
+        return nonSellableItemsList.get(nIndex).getPrice();
+    }
+
+    public int getItemCaloricValue(int nIndex) {
+        return nonSellableItemsList.get(nIndex).getCaloricValue();
+    }
+
+    public int getItemQuantity(int nIndex) {
+        return nonSellableItemsList.get(nIndex).getQuantity();
+    }
+
+    public int getItemListSize() {
+        return nonSellableItemsList.size();
+    }
+
     public boolean Combine(String strName, SellableItem sellableItem, NonSellableItem nonSellableItem) {
         combineItemList.add(new CombineItem(strName, sellableItem, nonSellableItem));
         transactionSummary.add(new CombineItem(strName, sellableItem, nonSellableItem));
